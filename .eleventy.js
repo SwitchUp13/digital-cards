b06bc826f6e34ec9569ec078e5b5eb0b9ce5b881
Jack <129770427+SwitchUp13@users.noreply.github.com>
2025-06-15 15:18:06 -0500

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -2,5 +2,14 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addCollection("clients", function(collectionApi) {
     return collectionApi.getFilteredByGlob("clients/*.json");
   });
+
+  return {
+    dir: {
+      input: ".",        // adjust if using src/
+      includes: "_includes",
+      data: "_data",
+      output: "_site"
+    }
+  };
 };
 
